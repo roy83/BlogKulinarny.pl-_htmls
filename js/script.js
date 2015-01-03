997b739436041f384e13c775262a567f90a4d04d
roy83 <roy83@wp.pl>
2015-01-03 15:00:07 +0100

    header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 (function() {
-	
 
+//przepisanie title na class, dla submenu
+$('.sub-menu li a').each(function(){
+	var $this = $(this);
+	$this.parent().addClass($this.attr('title'));
+	$this.removeAttr('title');
+});
+
+//main-nav click effect
+$('.main-nav').find('a[href*="przepisy"]').on('click',function(e){
+	e.preventDefault();
+	$('.main-nav').find('.sub-menu').slideToggle();
+});
 	
 })();
